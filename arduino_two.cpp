c8c5d07ea0e7745247568eeb6ef048d8654118f0
pinhojuliana <studyjulianaa@gmail.com>
2024-11-05 13:03:07 -0300

    final commit

--- arduino_two.cpp
+++ arduino_two.cpp
@@ -1,80 +1,73 @@
-// Arduino 2
 #include <SPI.h>
+#include <ShiftRegister74HC595.h>
 
-// Definições de pinos
-const int latchPin = 10;  // Pino de travamento (latch)
-const int clockPin = 13;  // Pino de clock
-const int dataPin = 11;   // Pino de dados
 
-// Total de displays
-const int numDisplays = 6;
+ShiftRegister74HC595<8> sr3(8, 9, 10); 
+ShiftRegister74HC595<8> sr4(11, 12, 13); 
 
-// Comandos para comunicação
-char commands[numDisplays] = {'b', 'c', 'J', 'd', 'f', 'I'};
-int displayValues[numDisplays][5];  // Valores para os displays
+void displayDigit(int digit, int position, bool decimal) {
+  static const byte digitCodes[] = {
+    0b11000000, 
+    0b11111001, 
+    0b10100100,
+    0b10110000, 
+    0b10011001, 
+    0b10010010, 
+    0b10000010, 
+    0b11111000, 
+    0b10000000, 
+    0b10010000  
+  };
+
+  byte segmentPattern = digitCodes[digit];
+  if (decimal) {
+    segmentPattern &= 0b01111111; 
+  }
+
+  if (position < 8) {
+    sr3.set(position, segmentPattern);
+  } else {
+    sr4.set(position - 8, segmentPattern);
+  }
+}
 
 void setup() {
-  // Configurar pinos
-  pinMode(latchPin, OUTPUT);
-  pinMode(clockPin, OUTPUT);
-  pinMode(dataPin, OUTPUT);
-
   Serial.begin(115200);
-  Serial.println("Iniciando Arduino 2...");
+  sr3.setAllLow();
+  sr4.setAllLow();
 }
 
 void loop() {
-  if (Serial.available() > 0) {
-    String input = Serial.readStringUntil('\n');
-    char command = input.charAt(0);
+  if (Serial.available() >= 1) {
+    char command = Serial.read();
+    switch (command) {
+      case 'd':
+        displayDigit(0, 1, false);
+        displayDigit(3, 2, false);
+        displayDigit(0, 3, false);
+        displayDigit(1, 9, false);
+        displayDigit(4, 10, false);
+        displayDigit(0, 11, false);
+        break;  
 
-    // Processar entrada
-    for (int i = 0; i < numDisplays; i++) {
-      if (command == commands[i]) {
-        // Extrair os valores dos dígitos da string recebida
-        String valueStr = input.substring(1, 6);  // 5 dígitos
-        for (int j = 0; j < 5; j++) {
-          displayValues[i][j] = valueStr.charAt(j) - '0';
-        }
-        updateDisplay(i);
-      }
+      case 'f':
+        displayDigit(0, 1, false);
+        displayDigit(3, 2, false);
+        displayDigit(0, 3, false);
+        displayDigit(1, 9, false);
+        displayDigit(4, 10, false);
+        displayDigit(0, 11, false);
+        break;  
+
+      case 'I':
+        displayDigit(1, 0, false);
+        displayDigit(0, 2, false);
+        displayDigit(5, 4, false);
+        break;  
+
+      default:
+        Serial.println("Comando desconhecido");
+        break;
     }
   }
-}
-
-// Função para atualizar o display
-void updateDisplay(int displayIndex) {
-  digitalWrite(latchPin, LOW);
-  
-  switch (displayIndex) {
-    case 0:  // Display com 5 dígitos (comando 'b')
-      for (int digit = 0; digit < 5; digit++) {
-        shiftOut(dataPin, clockPin, MSBFIRST, displayValues[displayIndex][digit]);
-      }
-      break;
-
-    case 1: case 2:  // Display com 4 dígitos (comando 'c' e 'J')
-      for (int digit = 1; digit < 5; digit++) {
-        shiftOut(dataPin, clockPin, MSBFIRST, displayValues[displayIndex][digit]);
-      }
-      break;
-
-    case 3: case 4:  // Display com 3 dígitos (contando do segundo dígito)
-      shiftOut(dataPin, clockPin, MSBFIRST, 0);  // Primeiro dígito desligado
-      for (int digit = 1; digit < 4; digit++) {
-        shiftOut(dataPin, clockPin, MSBFIRST, displayValues[displayIndex][digit]);
-      }
-      shiftOut(dataPin, clockPin, MSBFIRST, 0);  // Último dígito desligado
-      break;
-
-    case 5:  // Display com 3 dígitos, pulando o 2º e 4º dígitos
-      shiftOut(dataPin, clockPin, MSBFIRST, displayValues[displayIndex][0]);
-      shiftOut(dataPin, clockPin, MSBFIRST, 0);  // 2º dígito desligado
-      shiftOut(dataPin, clockPin, MSBFIRST, displayValues[displayIndex][2]);
-      shiftOut(dataPin, clockPin, MSBFIRST, 0);  // 4º dígito desligado
-      shiftOut(dataPin, clockPin, MSBFIRST, displayValues[displayIndex][4]);
-      break;
-  }
-  
-  digitalWrite(latchPin, HIGH);
 }
