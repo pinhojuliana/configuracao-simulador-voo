e1f3377333d0fa28a8730f94e2b1aded87889e35
pinhojuliana <studyjulianaa@gmail.com>
2024-11-05 13:12:27 -0300

    correção de erros

--- arduino_one.cpp
+++ arduino_one.cpp
@@ -1,62 +1,98 @@
-// Arduino 1
 #include <SPI.h>
+#include <ShiftRegister74HC595.h>
 
-// Definições de pinos
-const int latchPin = 10;  // Pino de travamento (latch)
-const int clockPin = 13;  // Pino de clock
-const int dataPin = 11;   // Pino de dados
+ShiftRegister74HC595<8> sr1(2, 3, 4); 
+ShiftRegister74HC595<8> sr2(5, 6, 7); 
 
-// Total de displays
-const int numDisplays = 8;
+void displayDigit(int digit, int position, bool decimal) {
+  static const byte digitCodes[] = {
+    0b11000000, 
+    0b11111001, 
+    0b10100100, 
+    0b10110000, 
+    0b10011001, 
+    0b10010010, 
+    0b10000010, 
+    0b11111000, 
+    0b10000000, 
+    0b10010000  
+  };
 
-// Comandos para comunicação
-char commands[numDisplays] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H'};
-int displayValues[numDisplays][5];  // Valores para os displays
+  byte segmentPattern = digitCodes[digit];
+  if (decimal) {
+    segmentPattern &= 0b01111111; 
+  }
+
+  if (position < 8) {
+    sr1.set(position, segmentPattern);
+  } else {
+    sr2.set(position - 8, segmentPattern);
+  }
+}
 
 void setup() {
-  // Configurar pinos
-  pinMode(latchPin, OUTPUT);
-  pinMode(clockPin, OUTPUT);
-  pinMode(dataPin, OUTPUT);
-
   Serial.begin(115200);
-  Serial.println("Iniciando Arduino 1...");
+  sr1.setAllLow();
+  sr2.setAllLow();
 }
 
 void loop() {
-  if (Serial.available() > 0) {
-    String input = Serial.readStringUntil('\n');
-    char command = input.charAt(0);
-
-    // Processar entrada
-    for (int i = 0; i < numDisplays; i++) {
-      if (command == commands[i]) {
-        // Extrair os valores dos dígitos da string recebida
-        String valueStr = input.substring(1, 6);  // 5 dígitos
-        for (int j = 0; j < 5; j++) {
-          displayValues[i][j] = valueStr.charAt(j) - '0';
+  if (Serial.available() >= 1) {
+    char command = Serial.read();
+    switch (command) {
+      case 'A':
+      case 'B':
+      case 'C':
+      case 'D':
+      case 'E':
+      case 'F':
+      case 'G':
+      case 'H':
+        for (int i = 0; i < 8; i++) {
+          displayDigit(1, i * 5 + 0, false);
+          displayDigit(2, i * 5 + 1, false);
+          displayDigit(3, i * 5 + 2, true);  
+          displayDigit(4, i * 5 + 3, false);
+          displayDigit(5, i * 5 + 4, false);
         }
-        updateDisplay(i);
-      }
+        break;
+        
+      case 'b':
+        displayDigit(1, 0, false);
+        displayDigit(2, 1, false);
+        displayDigit(3, 2, false);
+        displayDigit(4, 3, false);
+        displayDigit(5, 4, false);
+        break;
+
+      case 'c':
+      case 'J':
+        displayDigit(1, 0, false);
+        displayDigit(2, 1, false);
+        displayDigit(3, 2, false);
+        displayDigit(4, 3, false);
+        break;
+
+      case 'd':
+      case 'f':
+       sr1.set(0, 0xFF); 
+        displayDigit(2, 1, false);
+        displayDigit(3, 2, false);
+        displayDigit(4, 3, false);
+        sr1.set(4, 0xFF);
+        break;
+
+      case 'I':
+        displayDigit(1, 0, false);
+        sr1.set(1, 0xFF); 
+        displayDigit(3, 2, false);
+        sr1.set(3, 0xFF); 
+        displayDigit(5, 4, false);
+        break;
+
+      default:
+        Serial.println("Comando não reconhecido");
+        break;
     }
   }
-}
-
-// Função para atualizar o display
-void updateDisplay(int displayIndex) {
-  digitalWrite(latchPin, LOW);
-  
-  for (int digit = 0; digit < 5; digit++) {
-    int value = displayValues[displayIndex][digit];
-    shiftOut(dataPin, clockPin, MSBFIRST, value);  // Envia os valores para o 74HC595
-
-    // Configura o ponto decimal no terceiro dígito
-    if (digit == 2) {
-      shiftOut(dataPin, clockPin, MSBFIRST, 0b10000000);  // Ponto decimal ativado
-    } else {
-      shiftOut(dataPin, clockPin, MSBFIRST, 0b00000000);  // Sem ponto decimal
-    }
-  }
-  
-  digitalWrite(latchPin, HIGH);
 }
